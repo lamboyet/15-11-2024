fc2f7d9b8523286b9372e59082728c89f897c6e4
lamboyet <150907153+lamboyet@users.noreply.github.com>
2024-11-15 23:57:19 +0300

    Add files via upload

---
+++ quickstart/quickstart/src/main/java/com/sebispringprojects/quickstart/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.sebispringprojects.quickstart;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+
+    @GetMapping(path = "hello")
+    public String helloWorld()
+    {return "Hello world";}
+}
